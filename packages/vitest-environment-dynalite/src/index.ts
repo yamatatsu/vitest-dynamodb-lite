740734f5a9b81453437524b53ecc4245c640cf18
yamatatsu <the.kinnikuman@gmail.com>
2024-04-26 00:40:26 +0900

    chore: remove console.time()

--- packages/vitest-environment-dynalite/src/index.ts
+++ packages/vitest-environment-dynalite/src/index.ts
@@ -7,25 +7,15 @@ import { createTables, deleteTables } from "./db";
 setEnvironmentVariables("");
 
 beforeAll(async () => {
-  console.time("start_dynalite");
   await start();
-  console.timeEnd("start_dynalite");
-  console.time("createTables");
   await createTables();
-  console.timeEnd("createTables");
 });
 
 afterEach(async () => {
-  console.time("deleteTables");
   await deleteTables();
-  console.timeEnd("deleteTables");
-  console.time("createTables");
   await createTables();
-  console.timeEnd("createTables");
 });
 
 afterAll(async () => {
-  console.time("stop_dynalite");
   await stop();
-  console.timeEnd("stop_dynalite");
 });
